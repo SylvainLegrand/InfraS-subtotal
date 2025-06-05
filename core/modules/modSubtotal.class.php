621121f422386dda9fc26d1050396c1371044c3f
Sylvain Legrand <sylvain.legrand@infras.fr>
2025-06-05 06:28:41 +0300

    v3.28.4 from ATM

--- core/modules/modSubtotal.class.php
+++ core/modules/modSubtotal.class.php
@@ -69,7 +69,7 @@ class modSubtotal extends DolibarrModules
 		$this->description = "Module permettant d'ajouter des titres, sous-totaux et des sous-totaux intermédiaires dans un tableau ou une liste, tout en facilitant le déplacement fluide d'une ligne d'éléments d'un sous-total à un autre.";
 		// Possible values for version are: 'development', 'experimental' or version
 
-		$this->version = '3.28.36';
+		$this->version = '3.28.40';
 
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
