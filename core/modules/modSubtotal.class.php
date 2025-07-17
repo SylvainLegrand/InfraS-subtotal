ed4e7dcea5bbc6cddfed7b177fc3b9a6ca71a766
Sylvain Legrand <sylvain.legrand@infras.fr>
2025-07-17 16:28:13 +0300

    Update modSubtotal.class.php

--- core/modules/modSubtotal.class.php
+++ core/modules/modSubtotal.class.php
@@ -70,7 +70,7 @@ class modSubtotal extends DolibarrModules
 		// Possible values for version are: 'development', 'experimental' or version
 
 
-		$this->version = '3.29.1';	// InfraS change
+		$this->version = '3.29.2';	// InfraS change
 
 
 		// Url to the file with your last numberversion of this module
